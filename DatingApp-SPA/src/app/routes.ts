4d61da01d0bf1acb470dfabb09167808bfa0b1fd
unknown <spyros.gr21@gmail.com>
2020-06-29 22:58:26 +0300

    Routing Added

---
+++ DatingApp-SPA/src/app/routes.ts
@@ -0,0 +1,25 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MemberListComponent } from './member-list/member-list.component';
+import { MessagesComponent } from './messages/messages.component';
+import { ListsComponent } from './lists/lists.component';
+import { AuthGuard } from './_guards/auth.guard';
+
+
+export const appRoutes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'home', component: HomeComponent},
+    {
+        // an sto path eixa /dummy tote to route tha itan px /dummymembers
+        // twra einai sketo /members
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [AuthGuard],
+        children: [
+            {path: 'members', component: MemberListComponent},
+            {path: 'messages', component: MessagesComponent},
+            {path: 'lists', component: ListsComponent},
+        ]
+    },
+    {path: '**', redirectTo: 'home', pathMatch: 'full'},
+];
